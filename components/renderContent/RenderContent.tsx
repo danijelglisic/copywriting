6c44913c9e9f449b075da38003312dd90f997708
Danijel Glišić <danijel.glisic@grm.digital>
2023-04-02 17:17:07 +0200

    added banner section

--- components/renderContent/RenderContent.tsx
+++ components/renderContent/RenderContent.tsx
@@ -3,12 +3,22 @@ import {
   ILandingSection,
   IRichTextSection,
   IPhotoSlider,
+  IFreeConsultationBanner,
 } from "@/@types/generated/contentful";
 import LandingSection from "../landingSection/LandingSection";
 import RichTextSection from "../richTextSection/RichTextSection";
+import Carousel from "../carousel/Carousel";
+import FreeConsultationBanner from "../freeConsultationBanner/FreeConsultationBanner";
 
 interface RenderComponentProps {
-  sections: (ILandingSection | IRichTextSection | IPhotoSlider)[] | undefined;
+  sections:
+    | (
+        | ILandingSection
+        | IRichTextSection
+        | IPhotoSlider
+        | IFreeConsultationBanner
+      )[]
+    | undefined;
 }
 const RenderContent = ({ sections }: RenderComponentProps) => {
   if (!sections) return <div></div>;
@@ -22,6 +32,14 @@ const RenderContent = ({ sections }: RenderComponentProps) => {
       if (section.sys.contentType.sys.id === "richTextSection") {
         const richTextSection = section as IRichTextSection;
         return <RichTextSection key={id} props={richTextSection} />;
+      }
+      if (section.sys.contentType.sys.id === "photoSlider") {
+        const photoSlider = section as IPhotoSlider;
+        return <Carousel key={id} props={photoSlider} />;
+      }
+      if (section.sys.contentType.sys.id === "freeConsultationBanner") {
+        const consultationBanner = section as IFreeConsultationBanner;
+        return <FreeConsultationBanner key={id} props={consultationBanner} />;
       }
     });
   };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import "slick-carousel/slick/slick.css";
 import type { AppProps } from "next/app";
 import { Poppins } from "next/font/google";
 
