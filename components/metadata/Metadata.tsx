d580e380b9d31dd3ffb2640b0bc27964482eec6e
Danijel Glišić <danijel.glisic@grm.digital>
2023-06-24 00:00:19 +0200

    fixed h1 tags

--- components/metadata/Metadata.tsx
+++ components/metadata/Metadata.tsx
@@ -13,6 +13,7 @@ const Metadata = ({ title, description }: MetadataProps) => {
       <link rel="icon" href="/static/favicon.ico" />
       <meta name="description" content={description} key="desc" />
       <meta property="og:title" content={title} />
+      <meta property="og:type" content="website" />
       <meta property="og:description" content={description} />
       <meta property="og:image" content="../../assets/placeholder.jpg" />
     </Head>
--- components/videoLandingSection/VideoLandingSection.tsx
+++ components/videoLandingSection/VideoLandingSection.tsx
@@ -18,21 +18,20 @@ const VideoLandingSection = ({ props }: VideoLandingSectionProps) => {
         fill
         className="z-[-1] object-cover"
       />
-      <div className="container py-28 flex flex-col lg:flex-row gap-8 lg:gap-12 lg:[&>*]:w-1/2 justify-between items-center">
-        <div className="lg:hidden">
-          <h1 className="heading-1">{title}</h1>
-        </div>
-        <div className="relative rounded-2xl overflow-hidden w-full items-center aspect-video">
-          <iframe
-            src={youtubeVideoUrl}
-            className="absolute w-full h-full top-0 bottom-0 left-0 right-0"
-          />
-        </div>
-        <div className="flex flex-col gap-16">
-          <h1 className="heading-1 hidden lg:block">{title}</h1>
-          <div className="body-1">
-            {description && documentToReactComponents(description)}
+      <div className="container py-20">
+        <div className="flex flex-col lg:flex-row gap-8 lg:gap-12 lg:[&>*]:w-1/2 justify-between items-center">
+          <div className="flex flex-col gap-16">
+            <h1 className="heading-1">{title}</h1>
           </div>
+          <div className="relative rounded-2xl overflow-hidden w-full items-center aspect-video">
+            <iframe
+              src={youtubeVideoUrl}
+              className="absolute w-full h-full top-0 bottom-0 left-0 right-0"
+            />
+          </div>
+        </div>
+        <div className="body-1 mt-12">
+          {description && documentToReactComponents(description)}
         </div>
       </div>
     </div>
