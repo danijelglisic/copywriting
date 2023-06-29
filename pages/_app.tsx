9b61bf76cfacc2f25e5bb1d659a8b097851740ee
Danijel Glišić <danijel.glisic@grm.digital>
2023-06-29 17:37:44 +0200

    added pixel and ga4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,51 @@ import "@/styles/globals.css";
 import "slick-carousel/slick/slick.css";
 import type { AppProps } from "next/app";
 import { Poppins } from "next/font/google";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Script from "next/script";
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
+const PIXEL_ID = "920792645646784";
+const GA_MEASUREMENT_ID = "G-GJYR9B2GGN";
+
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    import("react-facebook-pixel")
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init(PIXEL_ID);
+        ReactPixel.pageView();
+
+        router.events.on("routeChangeComplete", () => {
+          ReactPixel.pageView();
+        });
+      });
+  }, [router.events]);
+
   return (
     <main className={poppins.className}>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_MEASUREMENT_ID}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
       <Component {...pageProps} />
     </main>
   );
