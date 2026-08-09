b227a76d7a97b2662c3d1e76d0741002bd201ca6
bogdanovicslavisa-cmd <bogdanovicslavisa@gmail.com>
2026-08-09 13:48:33 +0200

    Left-align white Video Ads carousel copy

--- components/renderContent/RenderContent.tsx
+++ components/renderContent/RenderContent.tsx
@@ -58,7 +58,7 @@ const RenderContent = ({
         const backgroundClass =
           regularSectionIndex % 2 === 0
             ? "[&>*]:!bg-[#F9F9F7] [&>*]:!text-dark"
-            : "[&>*]:!bg-white [&>*]:!text-left";
+            : "[&>*]:!bg-white [&>*>.container>.text-center]:!text-left";
         const renderVideoAdsSection = (content: React.ReactNode) => {
           if (!useVideoAdsRedesign || isConsultationBanner) return content;
 
